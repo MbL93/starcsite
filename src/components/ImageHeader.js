cb5dc40844a4c39de6c103ca48ed75bbdc5931d7
Marc Blickle <marcblickle@gmx.de>
2023-04-24 11:56:12 +0200

    x

--- src/components/ImageHeader.js
+++ src/components/ImageHeader.js
@@ -15,63 +15,63 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
 }
-const ImageHeader = ({ imageTitle}) => {
+const ImageHeader = ({ imageTitle }) => {
   return (
-    <ImageHeaderDiv>
-      <div
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "0",
-          ZIndex: "2",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-          opacity: "40%",
-          backgroundColor: "black",
-        }}
-      ></div>
-      <div
-        style={{
-          position: "absolute",
-          top: "0",
-          left: "0",
-          ZIndex: "39",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        {" "}
-        <div style={{ textAlign: "center", color: "white" }}>
-          <Headline>
-            {imageTitle}
-          </Headline>
-        </div>
-      </div>
-
-      <div style={{ position: "relative" }}>
-        <Slider {...settings}>
-          <StaticImage
-            src="../assets/images/header.jpg"
-            alt="header"
-            placeholder="tracedSVG"
-            layout="fullWidth"
-            as="div"
-          />
-        </Slider>
-      </div>
-    </ImageHeaderDiv>
+    <ImageHeaderDiv></ImageHeaderDiv>
+    // <ImageHeaderDiv>
+    //   <div
+    //     style={{
+    //       position: "absolute",
+    //       top: "0",
+    //       left: "0",
+    //       ZIndex: "2",
+    //       display: "flex",
+    //       justifyContent: "center",
+    //       alignItems: "center",
+    //       position: "absolute",
+    //       top: "0",
+    //       left: "0",
+    //       width: "100%",
+    //       height: "100%",
+    //       opacity: "40%",
+    //       backgroundColor: "black",
+    //     }}
+    //   >
+    //     {" "}
+    //     <div style={{ position: "relative", zIndex: "1" }}>
+    //       <Slider {...settings}>
+    //         <StaticImage
+    //           src="../assets/images/header.jpg"
+    //           alt="header"
+    //           placeholder="tracedSVG"
+    //           layout="fullWidth"
+    //           as="div"
+    //         />
+    //       </Slider>
+    //     </div>
+    //   </div>
+    //   <div
+    //     style={{
+    //       position: "absolute",
+    //       top: "0",
+    //       left: "0",
+    //       ZIndex: "39",
+    //       display: "flex",
+    //       justifyContent: "center",
+    //       alignItems: "center",
+    //       position: "absolute",
+    //       top: "0",
+    //       left: "0",
+    //       width: "100%",
+    //       height: "100%",
+    //     }}
+    //   >
+    //     {" "}
+    //     <div style={{ textAlign: "center", color: "white" }}>
+    //       <Headline>{imageTitle}</Headline>
+    //     </div>
+    //   </div>
+    // </ImageHeaderDiv>
   )
 }
 
