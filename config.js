2b88346b562001b5244d4b298c10e459b090e0be
Marc Blickle <marcblickle@gmx.de>
2023-06-15 18:36:00 +0200

    kp

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  siteTitle: "Starci Photos",
-}
--- src/styles/GalleryLayout.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-const GalleryContainer = styled.div`
-  margin-bottom: 50px;
-`
-
-export { GalleryContainer }
